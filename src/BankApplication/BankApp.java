40b5acd5f4d72ba702f4dc9b6d9d683ac3bda8bd
Victoria Cherotich <Victoria.Cherotich@ncbagroup.com>
2022-06-30 11:23:57 +0300

    my project

---
+++ src/BankApplication/BankApp.java
@@ -0,0 +1,12 @@
+package BankApplication;
+
+public class BankApp {
+
+	public static void main(String[] args) {
+		
+			BankApplication obj1 = new BankApplication("Victoria", "499169");
+			
+			obj1.displayMenu();			
+	}
+
+}
---
+++ src/BankApplication/BankApplication.java
@@ -0,0 +1,80 @@
+package BankApplication;
+
+import java.util.Scanner;
+
+public class BankApplication {
+	String cname;
+	String cid;
+	int deposit;
+	int withdraw;
+	int balance = 5000;
+	int previousTransaction;
+
+	// Constructor to get customer name and customer id
+	BankApplication(String cname, String cid) {
+		this.cname = cname;
+		this.cid = cid;
+		System.out.println("Welcome" + " " + this.cname + " " + this.cid);
+	}
+	void displayMenu() {
+		System.out.println("1. View Balance");
+		System.out.println("2. Deposit cash");
+		System.out.println("3. Withdraw cash");
+		System.out.println("4. get previous transaction");
+		System.out.println("0 Exit");
+		System.out.println("Select 1 option");
+
+		try (Scanner in = new Scanner(System.in)) {
+			int menuChoice = in.nextInt();
+			if (menuChoice == 1) {
+				balance();
+			} else if (menuChoice == 2) {
+				depositCash();
+			} else if (menuChoice == 3) {
+				withdrawCash();
+			} else if (menuChoice == 4) {
+				exit();
+			}
+
+			else {
+				System.out.println("Please select an option");
+			}
+		}
+
+	}
+
+	void depositCash() {
+		try (Scanner depAmount = new Scanner(System.in)) {
+			System.out.println("Enter amount you want to deposit");
+			int depoAmount = depAmount.nextInt();
+			System.out.println("Amount deposited is" + depoAmount);
+			balance = balance + depoAmount;
+			System.out.println("new balance" + balance);
+			displayMenu();
+		}
+
+	}
+
+	void withdrawCash() {
+		try (Scanner withAmount = new Scanner(System.in)) {
+			System.out.println("Enter amount you want to withdraw");
+			int withdAmount = withAmount.nextInt();
+			System.out.println("Amount withdrawn is" + withdAmount);
+
+			balance = balance - withdAmount;
+			System.out.println("new balance" + balance);
+			displayMenu();
+		}
+	}
+	void balance() {
+		System.out.println("Your balance is" + balance);
+		displayMenu();
+	}
+
+	void exit() {
+		System.out.println("Thank You for visiting our bank.Welcome again");
+		
+
+	}
+
+}
